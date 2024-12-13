c83c8de8aaffdb15e2c00e51a25225f303f8e071
lucasDrzt <115454870+lucasDrzt@users.noreply.github.com>
2024-12-13 15:35:20 +0100

    Update bonjour.asm

--- bonjour file/bonjour.asm
+++ bonjour file/bonjour.asm
@@ -5,41 +5,36 @@ section .data
     buffer db 50                       ; Espace pour le nom (50 octets max)
 
 section .bss
-    input resb 50                      ; Réserve de la mémoire pour l'entrée utilisateur
+    input resb 50                      
 
 section .text
-    global _start                      ; Point d'entrée du programme
+    global _start                      
 
 _start:
-    ; Afficher le message "entre ton nom: "
-    mov eax, 4                         ; syscall: write
-    mov ebx, 1                         ; fichier: stdout
-    mov ecx, prompt                    ; adresse du message
-    mov edx, prompt_len                ; taille du message
-    int 0x80                           ; appel système
+    mov eax, 4                         
+    mov ebx, 1                         
+    mov ecx, prompt                    
+    mov edx, prompt_len                
+    int 0x80                           
 
-    ; Lire l'entrée utilisateur
-    mov eax, 3                         ; syscall: read
-    mov ebx, 0                         ; fichier: stdin
-    mov ecx, input                     ; adresse pour stocker l'entrée
-    mov edx, 50                        ; taille maximum
-    int 0x80                           ; appel système
+    mov eax, 3                         
+    mov ebx, 0                         
+    mov ecx, input                     
+    mov edx, 50                        
+    int 0x80                           
 
-    ; Afficher "bonjour, "
-    mov eax, 4                         ; syscall: write
-    mov ebx, 1                         ; fichier: stdout
-    mov ecx, greeting                  ; adresse du message d'accueil
-    mov edx, 9                         ; taille du message (longueur de "bonjour, ")
-    int 0x80                           ; appel système
+    mov eax, 4                         
+    mov ebx, 1                         
+    mov ecx, greeting                  
+    mov edx, 9                         
+    int 0x80                           
 
-    ; Afficher le nom saisi
-    mov eax, 4                         ; syscall: write
-    mov ebx, 1                         ; fichier: stdout
-    mov ecx, input                     ; adresse de l'entrée utilisateur
-    mov edx, 50                        ; taille maximum (ou ajustez pour enlever '\n')
-    int 0x80                           ; appel système
+    mov eax, 4                         
+    mov ebx, 1                         
+    mov ecx, input                     
+    mov edx, 50                        
+    int 0x80                          
 
-    ; Quitter le programme
-    mov eax, 1                         ; syscall: exit
-    xor ebx, ebx                       ; code de sortie: 0
-    int 0x80                           ; appel système
+    mov eax, 1                         
+    xor ebx, ebx                       
+    int 0x80                           
